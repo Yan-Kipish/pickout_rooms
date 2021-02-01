919d644501ad69b46ba20c61d72a37e68ff65929
Yan-Kipish <gleb945@gmail.com>
2021-02-01 21:39:11 +0200

    get_or_create() & add_user() updated

--- db.py
+++ db.py
@@ -24,23 +24,28 @@ def session_scope():
     finally:
         session.close()
 
-def add_user(user: schemas.UserSocket):
-    new_user_dict = user.dict()
-    input_user = models.User(
-        nickname = new_user_dict['user']['nickname'],
-        phone = new_user_dict['user']['phone']
-    )
-    input_messenger = models.Messenger(
-        messenger_name=new_user_dict['messenger']['messenger_name']
-    )
-    input_chat_token = new_user_dict['chat_token']
+def get_or_create(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
 
+    if not instance:
+        instance = model(**kwargs)
+        session.add(instance)
+
+    return instance
+
+def add_user(user_in: schemas.UserSocket):
     with session_scope() as session:
-        messenger = session.query(models.Messenger).filter_by(
-            messenger_name=input_messenger.messenger_name
+        input_user = get_or_create(
+            session, 
+            models.User, 
+            **user_in.user.dict()
         )
-        if messenger is None:
-            messenger = input_messenger
+        messenger = get_or_create(
+            session, 
+            models.Messenger,
+            **user_in.messenger.dict()
+        )
+        input_chat_token = user_in.chat_token
         session.add(models.UserSocket(
             user=input_user,
             messenger=messenger,
