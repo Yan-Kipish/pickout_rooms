19a3a5b7e65194ca55a75af04e10de7a1f994424
= <=>
2021-02-19 23:36:30 +0200

    ORM models updated

--- models.py
+++ models.py
@@ -4,32 +4,72 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-class User(Base):
+class Users(Base):
     """
     Модель пользователя
     """
     __tablename__ = 'users'
-    id = Column(Integer(), primary_key=True)
+    userId = Column(Integer(), primary_key=True)
     nickname = Column(String(24), nullable=False, unique=True)
     phone = Column(String(16), nullable=True)
 
-class Messenger(Base):
+class Settings(Base):
+    """
+    Глобальные настройки юзера
+    """
+    __tablename__ = 'settings'
+    settingsId = Column(Integer(), primary_key=True)
+    userId = Column(Integer(), ForeignKey('users.userId'), nullable=False)
+    settingName = Column(String(24), nullable=False)
+    value = Column(Text())
+
+class Messengers(Base):
     """
     Справочник мессенджеров
     """
     __tablename__ = 'messengers'
-    id = Column(Integer(), primary_key=True)
-    messenger_name = Column(String(32), nullable=False)
+    messengerId = Column(Integer(), primary_key=True)
+    messengerName = Column(String(32), nullable=False)
 
-class UserSocket(Base):
+class Accounts(Base):
     """
     Привязки юзеров к ботам (chat_id, token, etc.)
     """
-    __tablename__ = 'user_sockets'
-    id = Column(Integer(), primary_key=True)
-    user_id = Column(Integer(), ForeignKey('users.id'))
-    messenger_id = Column(Integer(), ForeignKey('messengers.id'))
-    chat_token = Column(Text(), nullable=False)
+    __tablename__ = 'accounts'
+    accountId = Column(Integer(), primary_key=True)
+    userId = Column(Integer(), ForeignKey('users.userId'), nullable=False)
+    messengerId = Column(Integer(), ForeignKey('messengers.messengerId'), nullable=False)
+    chatToken = Column(Text(), nullable=False)
+    priority = Column(Integer())
 
-    user = relationship("User", backref='sockets')
-    messenger = relationship("Messenger", backref='sockets')
+    user = relationship("Users", backref='accounts')
+    messenger = relationship("Messengers", backref='accounts')
+
+class Rooms(Base):
+    """
+    Те самые чатрумы =)
+    """
+    __tablename__ = 'rooms'
+    roomId = Column(Integer(), primary_key=True)
+    roomName = Column(String(48), nullable=False, unique=True)
+
+class RoomsUsers(Base):
+    roomId = Column(Integer(), ForeignKey('rooms.roomId'), primary_key=True)
+    userId = Column(Integer(), ForeignKey('users.userId'), primary_key=True)
+    roleId = Column(Integer(), ForeignKey('roles.roleId'))
+    markerId = Column(Integer(), ForeignKey('markers.markerId'))
+
+    users = relationship("Users")
+    rooms = relationship("Rooms")
+    roles = relationship("Roles")
+    markers = relationship("Markers")
+
+class Roles(Base):
+    __tablename__ = 'roles'
+    roleId = Column(Integer(), primary_key=True)
+    roleName = Column(String(48), nullable=False)
+
+class Markers(Base):
+    __tablename__ = 'markers'
+    markerId = Column(Integer(), primary_key=True)
+    markerName = Column(String(48), nullable=False)
